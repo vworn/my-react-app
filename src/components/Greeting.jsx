e5f08638ef58e5cf99ea27da74c2a227e9c9bd1f
vworn <jasonkmathewpersonal@gmail.com>
2025-04-06 17:08:43 -0400

    Task 1: Create a New Component

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Greeting = () => {
+  return (
+    <div>
+      <h2>Hello from the Greeting Component!</h2>
+      <p>This is your first custom component in React.</p>
+    </div>
+  );
+};
+
+export default Greeting;
